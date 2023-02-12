860501ae087754a84e6e0c03c50af5ccc2d54de2
breezmax <36204784+breezmax@users.noreply.github.com>
2023-02-12 17:36:39 +0100

    HW1 moved to Selenium folder

---
+++ Selenium/HW1/src/test/java/herokuapp/AddRemoveElementsTest.java
@@ -0,0 +1,33 @@
+package herokuapp;
+
+import framework.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class AddRemoveElementsTest extends BaseTest {
+
+    private static final String NAV_ADD_REMOVE_MENU = "//a[contains(text(),'%s')]";
+    private static final String ADD_ELEMENT_BUTTON = "//button[contains(text(), '%s')]";
+
+    private static final String REMOVE_ELEMENT_BUTTON = "//div[@id='elements']/button[2]";
+
+
+    @Test
+    public void AddRemoveTest(){
+        WebElement addRemoveMenu = driver.findElement(By.xpath(String.format(NAV_ADD_REMOVE_MENU, "Add/Remove Elements")));
+        addRemoveMenu.click();
+
+        WebElement addElementButton = driver.findElement(By.xpath(String.format(ADD_ELEMENT_BUTTON, "Add Element")));
+        addElementButton.click();
+        addElementButton.click();
+
+        WebElement removeElementButton = driver.findElement(By.xpath(REMOVE_ELEMENT_BUTTON));
+        removeElementButton.click();
+
+        System.out.printf("Final count of elements: %s\n", driver.findElements(By.className("added-manually")).size());
+
+
+
+    }
+}
